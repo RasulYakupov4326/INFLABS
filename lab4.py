243717f9194fd1d022fd198e38ba688d7b1ab3d6
RasulYakupov4326 <148913783+RasulYakupov4326@users.noreply.github.com>
2023-10-25 12:16:07 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,2 +1,11 @@
-import main
-main
+from main import major
+
+condition = True
+while condition:
+    print("Введите строку")
+    inputLine = input()
+    if len(inputLine.split()) != 0:
+        major(inputLine)
+        condition = False
+    else:
+        print("Введенная строка пустая")
